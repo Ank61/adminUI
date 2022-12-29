090a642c3c0ef48e4365794cbb3ae1e567875fcc
Ankit61 <ankitchohan61@gmail.com>
2022-12-29 17:09:42 +0530

    second commit

--- src/Components/Pages/mainPage/dashboard.js
+++ src/Components/Pages/mainPage/dashboard.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 export default function Dashboard() {
+    const [dept, setDept] = useState("")
     const [update, setUpdate] = useState("")
-    const [present,setpresent] = useState("")
-    const [Absent,setabsent] = useState("")
+    const [present, setpresent] = useState("")
+    const [Absent, setabsent] = useState("")
+    const [showtoday, setTodayShow] = useState(false);
 
     useEffect(() => {
         axios.get("http://localhost:3001/dashboard").then((response) => {
@@ -13,19 +17,27 @@ export default function Dashboard() {
 
 
         axios.get("http://localhost:3001/user").then((response) => {
-            const present = response.data.filter((item) =>item.status === "present");
-            const absent = response.data.filter(function(filteredInput){
-                return !present.find(function(input){
-                    return input.name ===filteredInput.name;
+            const present = response.data.filter((item) => item.status === "present");
+            const absent = response.data.filter(function (filteredInput) {
+                return !present.find(function (input) {
+                    return input.name === filteredInput.name;
                 });
             });
-            console.log(absent)
             setabsent(absent.length)
             setpresent(present.length)
-        })
-            .catch((err) => console.log(err))
+        }).catch((err) => console.log(err))
+
+        axios.get("http://localhost:3001/dashboard").then((response) => {
+            setDept(response.data[0])
+            console.log(response.data[0])
+        }).catch((err) => console.log(err))
     }, [])
 
+    const handleClose = () => setTodayShow(false);
+    const handleShow = () => setTodayShow(true);
+
+    var Assigned = 0
+    var Completed = 0
 
     return (
         <div>
@@ -39,8 +51,8 @@ export default function Dashboard() {
                     </ul>
                     <h6 className="Subheading">Today's Status</h6>
                     <ul className='notification'>
-                    <li><small>Total employee present : &nbsp; {present}  </small></li>
-                    <li><small>Total employee absent : &nbsp; {Absent}  </small></li>
+                        <li><small>Total employee present : &nbsp; {present}  </small></li>
+                        <li><small>Total employee absent : &nbsp; {Absent}  </small></li>
                     </ul>
                 </div>
                 <div className="Dash2">
@@ -68,7 +80,41 @@ export default function Dashboard() {
                     </ul>
                 </div>
             </div>
-            <div className="Dash3">Helllo Dash</div>
+            <div className="Dash3">
+                <div className='text_performance'>
+                    <h5>Performance</h5>
+                </div>
+                <div className='bda' onClick={handleShow}>
+                    <h6 style={{ marginLeft: 30 }}>This Day</h6>
+                    {dept.bda ? dept.bda.map((item) => {
+                        const completed = Number(item.completed)
+                        const assigned = Number(item.today)
+                        Assigned += assigned
+                        Completed += completed
+                    }) : ""}
+                    <ul style={{ paddingTop: 8 }}>
+                        <li><small>Assigned : {Assigned}</small></li>
+                        <li><small>Completed : {Completed}</small></li>
+                    </ul>
+                </div>
+                <Modal show={showtoday} onHide={handleClose} dialogClassName="modal-90w" centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title style={{ color: '#bd6b35', marginLeft: 30 }}>Buisness Development Associate</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <ul>
+                            {dept.bda ? dept.bda.map((item) => (<>
+                                <li style={{ paddingTop: 30 }}><b>Name &nbsp;:</b> &nbsp;{item.name}</li>
+                                <b>Assigned &nbsp;today &nbsp;:</b> &nbsp;{item.today} &nbsp;&nbsp; <b>Completed </b> &nbsp;: &nbsp;{item.completed}
+                            </>
+                            )) : ""}
+                        </ul>
+                    </Modal.Body>
+                    <Button className="closeButton" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal>
+            </div>
         </div>
     )
 }

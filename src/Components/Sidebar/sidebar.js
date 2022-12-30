39725c16632760408aee177abd0fda5d6644816d
Ankit61 <ankitchohan61@gmail.com>
2022-12-30 18:52:42 +0530

    profile

--- src/Components/Sidebar/sidebar.js
+++ src/Components/Sidebar/sidebar.js
@@ -2,10 +2,16 @@ import '../Pages/mainPage/mainPage.css'
 import { Button } from 'react-bootstrap';
 import Image from '../../Images/logo.jpeg';
 import {useNavigate} from "react-router-dom"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './sidebar.css';
 
 
 
 export default function Sidebar(){
+
+    const [admin,setAdmin] = useState({})
+
     const navigate =useNavigate()
 
     function handleLogo(){
@@ -20,9 +26,22 @@ export default function Sidebar(){
     function handleDailyTask(){
         navigate("/dailyTask")
     }
+
+useEffect(()=>{
+axios.get('http://localhost:3001/user').then((response)=>setAdmin(response.data)).catch((err)=>console.log(err))
+},[])
+
+
     return (
         <>
          <img src={Image} alt="Company Logo" style={{width:180,height: 110,paddingLeft:60}} onClick={()=>handleLogo()}></img>
+           <div>
+            <div className='profilePic'>
+
+            </div>
+         {admin.length>0?<h6 className='admin'>{admin[5].name}</h6>:""}
+         {admin.length>0?<h6 className='designation'>{admin[5].designation}</h6>:""}
+         </div>
                 <br></br>
                 <Button  variant="light" size="md" className='buttons' onClick={()=>handleDashboard()}><i class="fa-solid fa-chart-line"></i> &nbsp; Dashboard</Button>
                 <br></br>
